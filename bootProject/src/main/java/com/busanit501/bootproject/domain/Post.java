6a9135c55f045f0488addf1406189741685419dc
seongyob99 <seongyob99@naver.com>
2024-12-26 17:12:37 +0900

    Post 사진 url 추가

--- bootProject/src/main/java/com/busanit501/bootproject/domain/Post.java
+++ bootProject/src/main/java/com/busanit501/bootproject/domain/Post.java
@@ -37,6 +37,9 @@ public class Post extends BaseEntity{
     @Column(columnDefinition = "TEXT")
     private String content;
 
+    @Column(length = 300)
+    private String imageUrl;
+
     // 댓글과의 관계 설정 (게시글 삭제 시 댓글도 삭제), 테스트 할때 빌더 디폴트 넣음 나중에 빼셈
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
